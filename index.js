b7f2ead98b585ce9cc4c5f966d3378eec704623b
MatheusRosa800 <61835871+MatheusRosa800@users.noreply.github.com>
2021-09-23 12:21:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+//servidor 
+var express = require("express")
+var app = express()
+
+//config database com acesso ao mongoDBatlas
+var mongoose = require('mongoose')
+var conexao =  ()=>{
+    var caminho = mongoose.connect('mongodb+srv://awesome:lucasmalaman@fiapcluster.prnks.mongodb.net/mongoatlas?retryWrites=true&w=majority')
+}
+
+var schema = mongoose.Schema
+var depoimentos = new schema({
+    nome:String,
+    cargo:String,
+    mensagem:String
+})
+var documentos = mongoose.model('depoimentos', depoimentos)
+//fim das configuracoes do database
+
+var porta =  3636
+
+// configurações 
+app.set("view engine",'ejs')
+app.use(express.static('./'))
+
+//rota para abrir o arquivo index.ejs
+app.get('/',(req,res)=>{
+    conexao()
+    documentos.find().limit(3).sort({'_id':-1})
+        .then((documentos)=>{
+            console.log(documentos)
+            res.render('index', {documentos})
+        })
+})
+
+//ligar o servidor 
+app.listen(porta)
+  
